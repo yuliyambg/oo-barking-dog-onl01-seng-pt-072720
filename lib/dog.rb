8c5aa4946eb620faa89e038b6e46d08bc44376f5
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-06 23:30:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+  
+  def name= dogs_name
+    @dogs_name = dogs_name
+  end
+  
+  
+  def name
+    @dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
